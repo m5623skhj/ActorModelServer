27bdc2e3a4efc5964ee33cc5ab43df7a62454de7
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-06-10 02:07:48 +0900

    * 하위 모듈에서 패킷을 받아와서 상위 모듈의 함수를 호출하게 할 수 있도록 구성   * 함수 인자를 위한 디시리얼라이저 추가   * 액터가 스스로 자신의 함수를 호출해야 하는 구조이므로, 패킷 핸들러를 등록하여 메시지를 생성     * 생성된 메시지는 ProcessMessage()에서 처리하게 하여, 액터가 스스로 처리하는 것 처럼 보이게 하려는 의도임

--- ActorModelServer/ActorModelServer/Actor.h
+++ ActorModelServer/ActorModelServer/Actor.h
@@ -3,9 +3,82 @@
 #include <queue>
 #include <mutex>
 #include <utility>
-#include <array>
-#include <queue>
 #include "CoreType.h"
+#include "PacketManager.h"
+#include "NetServerSerializeBuffer.h"
+
+namespace Deserializer
+{
+	template<typename T>
+	T ReadPrimitive(const char*& data, size_t& remaining)
+	{
+		if (remaining < sizeof(T))
+			throw std::runtime_error("Deserialize: not enough data");
+		T value;
+		std::memcpy(&value, data, sizeof(T));
+		data += sizeof(T);
+		remaining -= sizeof(T);
+		return value;
+	}
+
+	inline std::string ReadString(const char*& data, size_t& remaining)
+	{
+		uint16_t length = ReadPrimitive<uint16_t>(data, remaining);
+		if (remaining < length)
+			throw std::runtime_error("Deserialize: string too long");
+		std::string str(reinterpret_cast<const char*>(data), length);
+		data += length;
+		remaining -= length;
+		return str;
+	}
+
+	template<typename T>
+	T DeserializeOne(const char*& data, size_t& remaining)
+	{
+		if constexpr (std::is_same_v<T, std::string>)
+			return ReadString(data, remaining);
+		else
+			return ReadPrimitive<T>(data, remaining);
+	}
+
+	template<typename... Args>
+	std::tuple<Args...> DeserializeImpl(const char*& data, size_t& remaining)
+	{
+		if constexpr (sizeof...(Args) == 0)
+		{
+			return std::tuple<>();
+		}
+		else if constexpr (sizeof...(Args) == 1)
+		{
+			return std::make_tuple(DeserializeOne<Args...>(data, remaining));
+		}
+		else
+		{
+			using FirstType = std::tuple_element_t<0, std::tuple<Args...>>;
+			FirstType first = DeserializeOne<FirstType>(data, remaining);
+
+			using RestTypes = decltype(std::apply([](auto, auto... rest)
+				{
+					return std::tuple<decltype(rest)...>{};
+				}, std::tuple<Args...>{}));
+
+			auto rest = std::apply([&](auto, auto... rest_args)
+				{
+					return DeserializeImpl<decltype(rest_args)...>(data, remaining);
+				}, std::tuple<Args...>{});
+
+			return std::tuple_cat(std::make_tuple(first), rest);
+		}
+	}
+
+	template<typename... Args>
+	std::tuple<Args...> Deserialize(NetBuffer& buffer)
+	{
+		const char* ptr = buffer.GetReadBufferPtr();
+		size_t remaining = buffer.GetUseSize();
+		return DeserializeImpl<Args...>(ptr, remaining);
+	}
+}
 
 class Actor
 {
@@ -43,4 +116,25 @@ protected:
 	std::queue<Message> storeQueue;
 
 	std::atomic_bool isStop{};
+
+public:
+	template<typename DerivedType, typename Func, typename... Args>
+	void RegisterPacketHandler(const PacketId type, Func DerivedType::* func)
+	{
+		messageFactories[type] = [func](Actor* actor, NetBuffer* packet) -> std::function<void()>
+			{
+				DerivedType* derived = static_cast<DerivedType*>(actor);
+				std::tuple<Args...> args = Deserializer::Deserialize<Args...>(*packet);
+				return [=]() {
+					std::apply([&](Args&&... unpackedArgs)
+						{
+							(derived->*func)(std::forward<Args>(unpackedArgs)...);
+						}, std::move(args));
+					};
+			};
+	}
+
+private:
+	using MessageFactory = std::function<std::function<void()>(Actor*, NetBuffer*)>;
+	std::unordered_map<PacketId, MessageFactory> messageFactories;
 };
